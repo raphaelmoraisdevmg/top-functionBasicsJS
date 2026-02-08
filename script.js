658dcafeb20a487fedfeb0083c529e992ad554fd
Raphael Almeida De Morais <raphaelmoraisdev@gmail.com>
2026-02-07 21:36:31 -0300

    solving numberThree requirement

--- script.js
+++ script.js
@@ -14,3 +14,17 @@ function multiply(a,b){
 result=multiply(3,3);//9
 result=multiply(9,9);//45
 //end of requirement 2
+
+//solving requirement 3
+function capitalize(text){
+  let lowerText =text.lowerCase(); //transform the given text to lowercase 
+  //capitalizing the fisrt character of transformed text.
+  return lowerText[0].toUpperCase() + lowerText 
+}
+//test cases for sugested requiriment 3
+let text = capitalize("abcd");
+console.log(text);//Abcd
+text = capitalize("ABCD");
+console.log(text);//Abcd
+ text = capitalize("aBcD");
+console.log(text);//Abcd
