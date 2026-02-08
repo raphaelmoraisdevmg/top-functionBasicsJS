2fc420fac43427d9d911f4f009d780539240586e
Raphael Almeida De Morais <raphaelmoraisdev@gmail.com>
2026-02-07 21:22:53 -0300

    solving requirement numberTwo

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// the requirements is in README.md file
 //solving requirement 1
 function addSeven(number){
   return number + 7;
@@ -5,3 +6,11 @@ function addSeven(number){
 let result = addSeven(10);
 console.log(result);//17
 //end of requirement 1
+
+//solving requirement 2
+function multiply(a,b){
+  return a * b;
+}
+result=multiply(3,3);//9
+result=multiply(9,9);//45
+//end of requirement 2
